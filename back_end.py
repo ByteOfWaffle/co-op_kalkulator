2ea0b05d8930435d2cbb306c10a3ed572860e6eb
Hoger0511 <hogersaeed0511@gmail.com>
2024-09-24 11:44:30 +0200

    Update back_end.py

--- back_end.py
+++ back_end.py
@@ -11,9 +11,21 @@ def trekkFra():
     diff = int(tall1) - int(tall2)
     print(f'{tall2} - {tall2} = {diff}')
 
+def dele():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    diff = int(tall1) / int(tall2)
+    print(f'{tall2} / {tall2} = {diff}')
+
+def gange():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    diff = int(tall1) * int(tall2)
+    print(f'{tall2} * {tall2} = {diff}')
+
+
 
 ''' Her er noen påbegynte tanker til utvidelse
 def gange():
     produkt = 3*3
     TODO: Gjøre ferdig '''
-skibidi
