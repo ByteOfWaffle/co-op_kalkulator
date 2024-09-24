0b080a018b60e16bcc88992068502c31dd0173ee
Hoger0511 <hogersaeed0511@gmail.com>
2024-09-24 13:11:49 +0200

    Update back_end.py

--- back_end.py
+++ back_end.py
@@ -27,8 +27,24 @@ def gange2():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
     tall3 = input("Skriv inn det tredje tallet: ")
-    diff = int(tall1) * int(tall2)
-    print(f'{tall1} * {tall2}  = {diff}') 
+    diff = int(tall1) * int(tall2) * int(tall3) 
+    print(f'{tall1} * {tall2} * {tall3}  = {diff}') 
+def gange3():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    tall3 = input("Skriv inn det tredje tallet: ")
+    tall4 = input("Srkiv inn det fjerde tallet: ")
+    diff = int(tall1) * int(tall2) * int(tall3) * int(tall4)
+    print(f'{tall1} * {tall2} * {tall3} * {tall4}  = {diff}') 
+def gange4():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    tall3 = input("Skriv inn det tredje tallet: ")
+    tall4 = input("Srkiv inn det fjerde tallet: ")
+    tall5 = input("Srkiv inn det femte tallet: ")
+    diff = int(tall1) * int(tall2) * int(tall3) * int(tall4) * int(tall5) 
+    print(f'{tall1} * {tall2} * {tall3} * {tall4} * {tall5}  = {diff}') 
+
 
 ''' Her er noen påbegynte tanker til utvidelse
 def gange():
