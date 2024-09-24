cbb1b71dad2ece8518c89825d384bd327200d63d
Hoger0511 <hogersaeed0511@gmail.com>
2024-09-24 13:41:22 +0200

    d

--- back_end.py
+++ back_end.py
@@ -1,52 +1,54 @@
+## Funksjon for å legge sammen to tall
 def leggSammen():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    sum = int(tall1) + int(tall2)
-    print(f'{tall1} + {tall2} = {sum}')
+    tall1 = input("Skriv inn det første tallet: ")  
+    tall2 = input("Skriv inn det andre tallet: ")   
+    sum = int(tall1) + int(tall2)  # Tallene konverteres til heltall og legges sammen
+    print(f'{tall1} + {tall2} = {sum}')  # Resultatet skrives ut
 
-
+# Funksjon for å trekke fra to tall
 def trekkFra():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    diff = int(tall1) - int(tall2)
-    print(f'{tall1} - {tall2} = {diff}')
+    tall1 = input("Skriv inn det første tallet: ")  
+    tall2 = input("Skriv inn det andre tallet: ")   
+    diff = int(tall1) - int(tall2)  # Tallene konverteres til heltall og trekkes fra hverandre
+    print(f'{tall1} - {tall2} = {diff}')  # Resultatet skrives ut
 
+# Funksjon for å dele to tall
 def dele():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    diff = int(tall1) / int(tall2)
-    print(f'{tall1} / {tall2} = {diff}')
+    tall1 = input("Skriv inn det første tallet: ") 
+    tall2 = input("Skriv inn det andre tallet: ")  
+    diff = int(tall1) / int(tall2)  # Tallene konverteres til heltall og deles
+    print(f'{tall1} / {tall2} = {diff}')  # Resultatet skrives ut
 
+# Funksjon for å gange to tall
 def gange():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    diff = int(tall1) * int(tall2)
-    print(f'{tall1} * {tall2}  = {diff}') 
+    tall1 = input("Skriv inn det første tallet: ")  
+    tall2 = input("Skriv inn det andre tallet: ")   
+    diff = int(tall1) * int(tall2)  # Tallene konverteres til heltall og multipliseres
+    print(f'{tall1} * {tall2}  = {diff}')  # Resultatet skrives ut
 
+# Funksjon for å gange tre tall
 def gange2():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    tall3 = input("Skriv inn det tredje tallet: ")
-    diff = int(tall1) * int(tall2) * int(tall3) 
-    print(f'{tall1} * {tall2} * {tall3}  = {diff}') 
+    tall1 = input("Skriv inn det første tallet: ")  
+    tall2 = input("Skriv inn det andre tallet: ")   
+    tall3 = input("Skriv inn det tredje tallet: ")  
+    diff = int(tall1) * int(tall2) * int(tall3)  # Tallene multipliseres
+    print(f'{tall1} * {tall2} * {tall3}  = {diff}')  # Resultatet skrives ut
+
+# Funksjon for å gange fire tall
 def gange3():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    tall3 = input("Skriv inn det tredje tallet: ")
-    tall4 = input("Srkiv inn det fjerde tallet: ")
-    diff = int(tall1) * int(tall2) * int(tall3) * int(tall4)
-    print(f'{tall1} * {tall2} * {tall3} * {tall4}  = {diff}') 
+    tall1 = input("Skriv inn det første tallet: ")  
+    tall2 = input("Skriv inn det andre tallet: ")   
+    tall3 = input("Skriv inn det tredje tallet: ")  
+    tall4 = input("Srkiv inn det fjerde tallet: ")  
+    diff = int(tall1) * int(tall2) * int(tall3) * int(tall4)  # Tallene multipliseres
+    print(f'{tall1} * {tall2} * {tall3} * {tall4}  = {diff}')  # Resultatet skrives ut
+
+# Funksjon for å gange fem tall
 def gange4():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    tall3 = input("Skriv inn det tredje tallet: ")
-    tall4 = input("Srkiv inn det fjerde tallet: ")
-    tall5 = input("Srkiv inn det femte tallet: ")
-    diff = int(tall1) * int(tall2) * int(tall3) * int(tall4) * int(tall5) 
-    print(f'{tall1} * {tall2} * {tall3} * {tall4} * {tall5}  = {diff}') 
-
-
-''' Her er noen påbegynte tanker til utvidelse
-def gange():
-    produkt = 3*3
-    TODO: Gjøre ferdig '''
+    tall1 = input("Skriv inn det første tallet: ")  # Brukeren skriver inn første tall
+    tall2 = input("Skriv inn det andre tallet: ")   # Brukeren skriver inn andre tall
+    tall3 = input("Skriv inn det tredje tallet: ")  # Brukeren skriver inn tredje tall
+    tall4 = input("Srkiv inn det fjerde tallet: ")  # Brukeren skriver inn fjerde tall
+    tall5 = input("Srkiv inn det femte tallet: ")   # Brukeren skriver inn femte tall
+    diff = int(tall1) * int(tall2) * int(tall3) * int(tall4) * int(tall5)  # Tallene multipliseres
+    print(f'{tall1} * {tall2} * {tall3} * {tall4} * {tall5}  = {diff}')  # Resultatet skrives ut
